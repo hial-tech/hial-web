e214a4c9a64a23fef6c03ef2860155dd16ce8581
Archit Raj <arch10@ymail.com>
2025-01-10 03:16:52 +0530

    add deployment via actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
-    },
+      '@': '/src'
+    }
   },
+  base: '/hial-web',
   server: {
-    open: true,
-  },
+    open: true
+  }
 });
